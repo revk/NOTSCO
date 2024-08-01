c7df397beb3456c9f1a3b6f183cd1a90f24c3401
RevK <github@k.gg>
2024-08-01 10:08:25 +0100

    Key

--- notsco.sql
+++ notsco.sql
@@ -205,7 +205,8 @@ CREATE TABLE `tester` (
   KEY `clientid` (`clientid`),
   KEY `email` (`email`(255)),
   KEY `emailed` (`emailed`),
-  KEY `lastlogin` (`lastlogin`)
+  KEY `lastlogin` (`lastlogin`),
+  KEY `apikey` (`apikey`(255))
 ) ENGINE=InnoDB AUTO_INCREMENT=694 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -218,4 +219,4 @@ CREATE TABLE `tester` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-01 10:07:56
+-- Dump completed on 2024-08-01 10:08:24
