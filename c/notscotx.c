b334215e52bffa7e7a46ddaae6f0607f9f000319
RevK <github@k.gg>
2024-08-27 08:05:48 +0100

    UPRN arbitrary text for testing

--- c/notscotx.c
+++ c/notscotx.c
@@ -54,7 +54,7 @@ residentialSwitchMatchRequest (SQL * sqlp, SQL_RES * res, j_t tx)
    if (*(val = sql_colz (res, "account")))
       j_store_string (payload, "account", val);
    j_t address = j_store_object (payload, "address");
-   if (strtoull (val = sql_colz (res, "uprn"), NULL, 10))
+   if (*(val = sql_colz (res, "uprn")))
       j_store_string (address, "uprn", val);
    j_t lines = j_store_array (address, "addressLines");
    if (*(val = sql_colz (res, "address1")))
